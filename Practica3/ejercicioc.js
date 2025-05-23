31353601c14ec21b223bf9275add65db2fafbc9d
IguanoVP <122044076@upq.edu.mx>
2025-05-23 16:08:50 -0600

    Ejercicio c

---
+++ Practica3/ejercicioc.js
@@ -0,0 +1,12 @@
+function simularPeticionAPI() { 
+return new Promise(resolve => { 
+setTimeout(() => { 
+resolve("Datos recibidos correctamente"); 
+}, 5000); 
+}); 
+} 
+async function obtenerDatos() { 
+let resultado = await simularPeticionAPI();
+console.log(resultado); 
+} 
+obtenerDatos();
